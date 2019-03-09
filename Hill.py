4a238fdbc3ed8fa7d41dd45212914b9eae3520ec
ERROR404 <32724548+hebtuerror404@users.noreply.github.com>
2019-03-09 17:26:51 +0800

    Code update

---
+++ Hill.py
@@ -0,0 +1,59 @@
+# -*- coding: UTF-8 -*-
+from sympy import *
+
+
+def hill_encrypt(plain_text, key, n):
+    plain_text = ''.join(plain_text.split())
+    key_data = []
+    row_list = []
+    for i in range(len(key)):
+        row_list.append(key[i])
+        if len(row_list) == n:
+            key_data.append(row_list)
+            row_list = []
+    key_matrix = Matrix(key_data)
+    plaintext_list = []
+    for i in plain_text:
+        if i.isupper():
+            plaintext_list.append(ord(i) - ord('A'))
+        else:
+            plaintext_list.append(ord(i) - ord('a'))
+    plaintext_matrix = Matrix([plaintext_list])
+    enctext_matrix = plaintext_matrix * key_matrix
+    enctext_list = list(enctext_matrix)
+    enc_text = ""
+    for i in enctext_list:
+        enc_text += chr(i % 26 + ord('a'))
+    return enc_text
+
+
+def hill_decrypt(enc_text, key, n):
+    enc_text = ''.join(enc_text.split())
+    key_data = []
+    row_list = []
+    for i in range(len(key)):
+        row_list.append(key[i])
+        if len(row_list) == n:
+            key_data.append(row_list)
+            row_list = []
+    key_matrix = Matrix(key_data).inv_mod(26)
+    enctext_list = []
+    for i in enc_text:
+        if i.isupper():
+            enctext_list.append(ord(i) - ord('A'))
+        else:
+            enctext_list.append(ord(i) - ord('a'))
+    enctext_matrix = Matrix([enctext_list])
+    plaintext_matrix = enctext_matrix * key_matrix
+    plaintext_list = list(plaintext_matrix)
+    plain_text = ""
+    for i in plaintext_list:
+        plain_text += chr(i % 26 + ord('a'))
+    return plain_text
+
+
+def hill_info():
+    print("---------------------------------------------Hill密码简介---------------------------------------")
+    print("希尔密码是运用基本矩阵论原理的替换密码，由Lester S. Hill在1929年发明。")
+    print("每个字母当作26进制数字：A=0, B=1, C=2... 一串字母当成n维向量，跟一个n×n的矩阵相乘，再将得出的结果模26。")
+    print("注意用作加密的矩阵（即密匙）在Z26必须是可逆的，否则就不可能解码。只有矩阵的行列式和26互质，才是可逆的。")
--- choice_main.py
+++ choice_main.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-import Playfair,Vigenere
+import Playfair,Vigenere,Hill
 
 
 def playfair_menu():
@@ -52,7 +52,7 @@ def vigenere_menu():
         print("4.返回上一级")
         crypto_operating = input("->")
         if crypto_operating == '1':
-            Vigenere.vigenere__info()
+            Vigenere.vigenere_info()
         elif crypto_operating == '2':
             print("-----------------Vigenere密码加密---------------")
             print("请输入您的明文：")
@@ -77,19 +77,62 @@ def vigenere_menu():
             print("选择出错！")
 
 
+def hill_menu():
+    while True:
+        print("-----Hill密码-----")
+        print("请选择你要进行的操作：")
+        print("1.Hill密码简介")
+        print("2.Hill密码加密")
+        print("3.Hill密码解密")
+        print("4.返回上一级")
+        crypto_operating = input("->")
+        if crypto_operating == '1':
+            Hill.hill_info()
+        elif crypto_operating == '2':
+            print("----------------------Hill密码加密----------------------")
+            print("请输入您的明文：（加密结束后会丢失大小写与空格）")
+            plain_text = input()
+            print("请输入您的密钥：(密钥应为一系列数字，空格分隔，依次输入)")
+            key = input()
+            print("请输入您的密钥阶数：")
+            n = input()
+            print("加密正在进行。。。")
+            print("加密成功！")
+            print("密文为：" + Hill.hill_encrypt(plain_text, key.split(),int(n)))
+        elif crypto_operating == '3':
+            print("----------------------Hill密码加密----------------------")
+            print("请输入您的密文：")
+            enc_text = input()
+            print("请输入您的密钥：(密钥应为一系列数字，空格分隔，依次输入)")
+            print("(密钥应为加密时使用的密钥)")
+            key = input()
+            print("请输入您的密钥阶数：")
+            n = input()
+            print("解密正在进行。。。")
+            print("解密成功！（大小写与空格需要自行根据语义恢复）")
+            print("明文为：" + Hill.hill_decrypt(enc_text, key.split(),int(n)))
+        elif crypto_operating == '4':
+            return
+        else:
+            print("选择出错！")
+
+
 def main_menu():
     while True:
         print("--------主菜单--------")
         print("请选择你要使用的密码：")
         print("1.Playfair密码")
         print("2.Vigenere密码")
-        print("3.退出")
+        print("3.Hill密码")
+        print("4.退出")
         crypto_type = input("->")
         if crypto_type == '1':
             playfair_menu()
         elif crypto_type == '2':
             vigenere_menu()
         elif crypto_type == '3':
+            hill_menu()
+        elif crypto_type == '4':
             return
         else:
             print("选择出错！")
